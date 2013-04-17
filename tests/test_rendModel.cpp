cec6cb6b0c6098a7ebd236a130b4eaf12fbe63ac
wg <williamgelhar@gmail.com>
2013-04-17 12:39:13 -0400

    "min" and "max" were replaced with the std variants because that makes so much more sense, now these two tests are useless so let's get rid of them.

--- tests/test_rendModel.cpp
+++ tests/test_rendModel.cpp
@@ -1,19 +1,2 @@
 #include "gtest/gtest.h"
 #include "rendModel.h"
-#include "../rendModel.cpp"
-
-TEST(TestMin, normal) {
-	EXPECT_EQ(1.0,min(1.0, 2.0));
-	EXPECT_EQ(1.0,min(2.0, 1.0));
-	EXPECT_EQ(1.0,min(1.0, 1.0));
-	EXPECT_EQ(-1.0,min(-1.0, 1.0));
-}
-
-TEST(TestMax, normal){
-    EXPECT_EQ(1.0, max(1.0, 0.0));
-    EXPECT_EQ(1.0, max(0.0, 1.0));
-	EXPECT_EQ(1.0, max(1.0, 1.0));
-    EXPECT_EQ(50.0, max(1.0, 50.0));
-    EXPECT_EQ(1.0, max(1.0, -1.0));
-    
-}
